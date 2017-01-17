c7c7a6a2d845352102822c75aebb0f04cac1da73
R45H <gmr45h@gmail.com>
2017-01-17 14:04:53 +0300

    Complete footer

--- app/src/blocks/aside/js-aside.js
+++ app/src/blocks/aside/js-aside.js
@@ -2,7 +2,51 @@ $(document).ready(function(){
 	var $aside = $('.js-aside'); // Боковое меню
 	var $toggle = $('.js-aside_toggle'); // Гамбургер для бокового меню
 
+	/*var arrMenu = [
+		{
+			title: 'All Categories',
+			icon: ' fa fa-reorder aside__toggle',
+			items: [
+				{
+					name: 'Devices',
+					link: '#',
+					items: [{
+						title: 'Devices',
+						icon: ' fa fa-laptop',
+						items: [
+							{
+								name: 'Mobile Phones',
+								link: '#'
+							},
+							{
+								name: 'Televisions',
+								link: '#'
+							},
+							{
+								name: 'Cameras',
+								link: '#'
+							}
+						]
+					}]
+				},
+				{
+					name: 'Magazines',
+					link: '#'
+				},
+				{
+					name: 'Collections',
+					link: '#'
+				},
+				{
+					name: 'Credits',
+					link: '#'
+				}
+			]
+		}
+	];*/
+
 	$aside.multilevelpushmenu({ // Инициализация бокового меню
+		//menu: arrMenu,
 		collapsed: true,
 		fullCollapse: true,
 		menuHeight: '100%',
