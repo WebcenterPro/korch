e20ca303acfa8b55a7224d81de663aa9e8ef6dda
R45H <gmr45h@gmail.com>
2017-01-25 17:27:04 +0300

    Complete the more-slider

---
+++ app/src/blocks/owl-more/owl-more.js
@@ -0,0 +1,36 @@
+$(function() {
+	var owlMore = $('.js-owl-more'); // Контейнер слайдера
+
+	owlMore.owlCarousel({
+		items: 4,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		dots: false,
+		nav: true,
+		navText: '',
+		margin: 30,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			992: {
+				items: 4
+			}
+		}
+	});
+
+	owlMore
+		.find('.owl-prev')
+		.append('<i class="fa fa-angle-left owl-more__arrow"></i>');
+
+	owlMore
+		.find('.owl-next')
+		.append('<i class="fa fa-angle-right owl-more__arrow"></i>');
+});
