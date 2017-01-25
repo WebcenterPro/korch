370b0fe45db76787fe18f2b55fcafd18ecf43a7f
R45H <gmr45h@gmail.com>
2017-01-25 17:39:30 +0300

    Added the fotorama

--- app/src/libs.js
+++ app/src/libs.js
@@ -2,3 +2,4 @@
 @@include('../libs/bootstrap/dist/js/bootstrap.js');
 @@include('../libs/owl.carousel/dist/owl.carousel.js');
 @@include('../libs/multilevelpushmenu/jquery.multilevelpushmenu.js');
+@@include('../libs/fotorama/fotorama.js');
