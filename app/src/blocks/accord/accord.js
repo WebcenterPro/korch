253cf9968c434c556e2583c50930c9a849e23b92
R45H <gmr45h@gmail.com>
2017-01-26 00:28:51 +0300

    Some fixes. Complete the accordion

--- app/src/blocks/accord/accord.js
+++ app/src/blocks/accord/accord.js
@@ -1,3 +1,23 @@
 $(function() {
+	var
+		isMoving = false, // Отображает, находится ли аккордеон в движении
+		accordHead = $('.accord__head'), // Шапки панелей аккордеона
+		headActiveClass = 'accord__head_active' // Класс активной шапки
+	;
 
+	accordHead.on('click', function() { // Действия при клике на шапку панели
+		if (isMoving) return;
+
+		var hasOpened = $(this).hasClass(headActiveClass);
+		accordHead.removeClass(headActiveClass);
+
+		if (!hasOpened) {
+			$(this).addClass(headActiveClass);
+		}
+
+		isMoving = true;
+		setTimeout(function() {
+			isMoving = false;
+		}, 350);
+	});
 });
