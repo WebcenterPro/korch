3fda06ea80703362e6968828ccaa88e42c01ef73
R45H <gmr45h@gmail.com>
2017-03-31 17:51:51 +0300

    Fixes

---
+++ app/src/blocks/owl-vert/owl-vert.js
@@ -0,0 +1,13 @@
+/* Вертикальный слайдер */
+$(function() {
+	$('.js-owl-vert').owlCarousel({
+		items: 1,
+		loop: true,
+		dots: true,
+		dotsClass: 'owl-vert__dots',
+		dotClass: "owl-vert__dot",
+		nav: false,
+		navText: ''
+	});
+});
+/* ========== */
